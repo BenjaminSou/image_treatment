de62c2bc12a93e224b941fa347ac39f6707581e1
BenajminSou <benjamin.soulan@depinfonancy.net>
2019-09-06 10:46:35 +0200

    UPTDATE Crontab

--- script_ftp.sh
+++ script_ftp.sh
@@ -1,18 +1,20 @@
 #!/bin/sh
 USERNAME="synftp"
 PASSWORD="ftpsyn!"
-SERVER="synapse"
+SERVER="synapse.meteo.fr"
 
 # local directory to pickup *.tar.gz file
-FILE="files/final"
+FILE="$MFDATA_CURRENT_PLUGIN_DIR/files/final"
 
 # remote server directory to upload backup
-BACKUPDIR="/poubelle_ia"
+BACKUPDIR="poubelle_ia"
+
+cd $FILE || exit
 
 # login to remote server
 ftp -n -i $SERVER <<EOF
 user $USERNAME $PASSWORD
 cd $BACKUPDIR
-mput $FILE/*.tar.gz
+mput *.tar
 quit
 EOF
